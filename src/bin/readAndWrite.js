013bc696160d44a3b926583266fbbccd9d25a5b6
hecate <hecateenitharmon@gmail.com>
2020-10-04 17:38:26 +0300

    feat: add error explanation on read and write

--- src/bin/readAndWrite.js
+++ src/bin/readAndWrite.js
@@ -1,28 +1,33 @@
 const fs = require('fs');
 
 const readAndWrite = (inputPath, outputPath, cypher, action, shift) => {
+    const errorDescription = (parameter, err) => `Program failed, ${parameter} parameter is invalid for some reasons: ${err}`;
+
+
     fs.readFile(inputPath, 'utf8' , (err, data) => {
         if (err) {
-            console.error(err);
-            return
+            const error = errorDescription('-i/--input', err);
+            console.error(error);
+            return;
         }
 
         const outputString = cypher(action, data, shift);
         fs.readFile(outputPath, 'utf8', (err, data) => {
             if (err) {
-                console.error(err);
-                return
+                const error = errorDescription('-o/--output', err);
+                console.error(error);
+                return;
             }
 
             const outputValue = data + outputString;
             fs.writeFile(outputPath, outputValue, err => {
                 if (err) {
-                    console.error(err);
-                    return
+                    const error = errorDescription('-o/--output', err);
+                    console.error(error);
                 }
-            })
+            });
         });
-    })
+    });
 };
 
 module.exports.readAndWrite = readAndWrite;
